e72f7e2e304d511e102596c363ea0ace4d846aff
Reyesgames <128988837+Reyesgames@users.noreply.github.com>
2023-11-11 13:33:31 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1699518205,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/spritefont.png",
+		"images/block-sheet0.png",
+		"images/player-sheet0.png",
+		"images/digismilespace-sheet0.png",
+		"images/speaker-sheet0.png",
+		"images/speaker-sheet1.png",
+		"images/taptostart-sheet0.png",
+		"images/highscore-sheet0.png",
+		"images/brickdodge-sheet0.png",
+		"media/gamemusic.m4a",
+		"media/gamemusic.ogg",
+		"media/gameover.m4a",
+		"media/gameover.ogg",
+		"media/menumusic.m4a",
+		"media/menumusic.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
